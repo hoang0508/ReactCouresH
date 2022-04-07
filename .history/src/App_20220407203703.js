696e50c27aca11133245da066130e1fe9c8f0ae1
hoang0508 <nguyenhuyhoang05082001@gmail.com>
2022-04-07 20:37:20 +0700

    h17-props-getter

---
+++ .history/src/App_20220407203703.js
@@ -0,0 +1,53 @@
+import React, { useState } from "react";
+import Switch from "./components/switch/Switch";
+
+function useTogle() {
+  const [on, setOn] = useState(false);
+  const toggle = () => setOn(!on);
+  // Props getter
+  function getToggleProps({ onClick, ...rest }) {
+    return {
+      onClick: () => {
+        onClick && onClick();
+        toggle();
+      },
+      ...rest,
+    };
+  }
+  return {
+    on,
+    toggle,
+    getToggleProps,
+    // Props collection
+    // toggleProps: {
+    //   onClick: toggle,
+    // },
+  };
+}
+// Props collection - Kentc Dodds - create of Remix
+// Props getter
+const App = () => {
+  const { on, toggleProps, getToggleProps } = useTogle();
+  // console.log("🚀 ~ file: App.js ~ line 19 ~ App ~ p", toggleProps);
+  return (
+    <>
+      {/* <Switch on={on} {...toggleProps} /> */}
+      <Switch {...getToggleProps({ on })} />
+      <hr />
+      <button
+        aria-label="custom-button"
+        {...getToggleProps({
+          onClick: () => console.info("onButtonToggle"),
+        })}
+        // onClick={() => {
+        //   console.log("onButton");
+        //   toggle();
+        // }}
+      >
+        {on ? "on" : "off"}
+      </button>
+    </>
+  );
+};
+
+export default App;
